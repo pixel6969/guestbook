91c0ec6158de832da0ad7319cd88e15dd596d448
pixel6969 <124809483+pixel6969@users.noreply.github.com>
2023-02-19 19:39:12 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Guestbook</title>
+    <title>guestbook</title>
 </head>
 <body>
-    <h1>Guestbook</h1>
+    <h1>guestbook</h1>
     <form method="post" action="process.php">
         Name: <input type="text" name="name"><br>
         Email: <input type="email" name="email"><br>
         Message: <br>
         <textarea name="message"></textarea><br>
-        <input type="submit" value="Submit">
+        <input type="submit" value="submit">
     </form>
+    <br>
+    <br>
+    <a href="viewguestbook.php">view guestbook</a>
 </body>
 </html>
